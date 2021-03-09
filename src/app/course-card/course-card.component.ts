fd758ed40c886fa4b20f1811daeefaabcd721147
Aditya Pratap <adityapratap7344@gmail.com>
2021-03-09 22:30:20 +0530

    completed UI of course-card

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-course-card',
-  templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.css']
+    selector: 'app-course-card',
+    templateUrl: './course-card.component.html',
+    styleUrls: ['./course-card.component.css'],
 })
 export class CourseCardComponent implements OnInit {
+    @Input() name: string = '';
+    @Input() image: string = '';
+    @Input() price: number = 0;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
